d6eee94ab3a9fcf8b942ef6a45a4f6c5f551b668
Vampo7152 <umangboss786@gmail.com>
2021-11-23 20:13:26 +0530

    Typo

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -29,7 +29,7 @@ function HeroSection() {
   <path d="M14 4.577v6.846L8 15l-6-3.577V4.577L8 1l6 3.577zM8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z"/>
 </svg>
             <span class="ml-4 flex items-start flex-col leading-none">
-              <span class="title-font font-medium">WT  -  2094</span>
+              <span class="title-font font-medium">WT  -  2094 lbs</span>
             </span>
           </button>
           </div>
